a4639425c0a150bb2618e616870b4f2955788ea2
unknown <skander.zoghlami@etu.univ-lyon1.fr>
2024-05-06 12:21:21 +0200

    Draw Triangle on the window (Hello Triangle)

--- main.cpp
+++ main.cpp
@@ -8,13 +8,12 @@ void framebuffer_size_callback(GLFWwindow *window, int width, int height)
     glViewport(0, 0, width, height);
 }
 
-
 // process input
-void processInput(GLFWwindow *window){
-    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 }
-
 
 int main()
 {
@@ -31,7 +30,7 @@ int main()
         glfwTerminate();
         return -1;
     }
-    // creating the context and assigning it to the window 
+    // creating the context and assigning it to the window
     glfwMakeContextCurrent(window);
     // we need to load the function pointers from the OpenGL driver before we use any OpenGL function
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
@@ -42,17 +41,67 @@ int main()
     glViewport(0, 0, 800, 600); // can be less than the window size
     // This line is when the user change the window size, the viewport will be changed
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f};
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+    // we create a VBO now, which is the memory space on the GPU First glObject we define
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    // 0 - Binding vertices in a buffer so that opengl can use it
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    const char *vertexShaderSource = "#version 330 core\n"
+                                     "layout (location = 0) in vec3 aPos;\n"
+                                     "void main()\n"
+                                     "{\n"
+                                     "gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                     "}\0";
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    const char *fragmentShaderSource = "#version 330 core\n"
+                                       "out vec4 FragColor;\n"
+                                       "void main()\n"
+                                       "{\n"
+                                       "FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+                                       "}\0";
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    // 1 - Set the vertex attributes pointers
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+
     while (!glfwWindowShouldClose(window))
     {
         // input
         processInput(window);
         // rendering commands here
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // state setting function
-        glClear(GL_COLOR_BUFFER_BIT); // state using function
+        glClear(GL_COLOR_BUFFER_BIT);         // state using function
         // swap buffers for double buffer, using double buffers avoid flickering by showing the front buffer and working on the back buffer until the rendering is done
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
     glfwTerminate();
     return 0;
 }
