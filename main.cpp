cf6fc0216a50d1f23bc68880f79bbe784c9361e0
unknown <skander.zoghlami@etu.univ-lyon1.fr>
2024-05-15 17:28:32 +0200

    Add GLM for matrix and vectors operations (Transformations)

--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@
 
 #include "shader.h"
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include <iostream>
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
