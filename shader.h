6d8a098bac0b2568c6accf4d7854b40eb9c1853d
unknown <skander.zoghlami@etu.univ-lyon1.fr>
2024-05-23 17:09:10 +0200

    Add new scene, 1 cube to elimunate, another is the light source, the light source has a white color, the box will be lit in orange it absorbs all other colors and emits orange (Colors)

--- shader.h
+++ shader.h
@@ -97,6 +97,10 @@ public:
     {
         glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
     }
+    void setVec3(const std::string &name, const glm::vec3 &value) const
+    {
+        glUniform3fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
+    }
 
 private:
     // utility function for checking shader compilation/linking errors.
