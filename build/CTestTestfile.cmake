f35b5994859f08722d7f0f731c56ae7180809383
skanderzoghlami <skander@infiniteorbits.io>
2025-05-05 15:50:12 +0200

    Started a new project clean

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/skander/OneDrive/Bureau/OpenGL with python/real_rendering
-# Build directory: C:/Users/skander/OneDrive/Bureau/OpenGL with python/real_rendering/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/Shaders/Stencil Testing/stencil.fs
+++
@@ -1,6 +0,0 @@
-#version 330 core
-
-out vec4 FragColor;
-void main() {
-    FragColor = vec4(0.85, 0.52, 0.19, 1.0);
-}
