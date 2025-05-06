752d80d11023e30de7f0c115e683defdd8f50929
skanderzoghlami <skander@infiniteorbits.io>
2025-05-06 14:25:32 +0200

    Iteration 2: Indexing the vertices and drawing them using the Element buffer object (EBO)

--- src/main.cpp
+++ src/main.cpp
@@ -35,12 +35,22 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-
 	GLfloat vertices[] =
 	{
 		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower left corner
 		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right corner
-		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f // Upper corner
+		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f, // Upper corner
+		-0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner left
+		0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner right
+		0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f // Inner down
+	};
+
+	// Indices for vertices order
+	GLuint indices[] =
+	{
+		0, 3, 5, // Lower left triangle
+		3, 2, 4, // Upper triangle
+		5, 4, 1 // Lower right triangle
 	};
 
     
@@ -83,21 +93,27 @@ int main() {
 
 
 
-    GLuint   VAO , VBO;
+    GLuint   VAO , VBO , EBO;
 
     glGenVertexArrays(1,&VAO); // 1 
     glGenBuffers(1, &VBO); // 2 order matters
+    glGenBuffers(1,&EBO) ; 
     
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER , sizeof(vertices) , vertices ,  GL_STATIC_DRAW);
 
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO) ;
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER , sizeof(indices) , indices ,  GL_STATIC_DRAW ) ;
+
     glVertexAttribPointer(0, 3 , GL_FLOAT , GL_FALSE , 3 * sizeof(float) , (void*)0  );
     glEnableVertexAttribArray(0);
     
     glBindBuffer(GL_ARRAY_BUFFER , 0);
     glBindVertexArray(0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
 
 
 
@@ -115,7 +131,7 @@ int main() {
 		// Bind the VAO so OpenGL knows to use it
 		glBindVertexArray(VAO);
 		// Draw the triangle using the GL_TRIANGLES primitive
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 9 ,GL_UNSIGNED_INT, 0);
 		// Swap the back buffer with the front buffer
 		glfwSwapBuffers(window);
 		// Take care of all GLFW events
@@ -123,6 +139,7 @@ int main() {
 	}
 
     glDeleteVertexArrays(1 , &VAO);
+    glDeleteBuffers(1, &EBO);
     glDeleteBuffers(1,&VBO) ;
     glDeleteProgram(shaderProgram);
     glfwDestroyWindow(window);
