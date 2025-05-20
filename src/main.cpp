e8824f1580eb2bbce3a9a021de4b43c4178789be
unknown <skander.zoghlami@etu.univ-lyon1.fr>
2025-05-20 16:36:46 +0200

    Change Camera class so that we can update the camera matrix on demand (fov/far and near plane) we need it to define light later, light can be seen as a special version of a camera

--- src/main.cpp
+++ src/main.cpp
@@ -138,7 +138,9 @@ int main()
 		shaderProgram.Activate();
 		// Going 3D with MVP
 		camera.Inputs(window,deltaTime);
-		camera.Matrix(45.0f, 0.1f, 100.0f, shaderProgram, "camMatrix");
+		camera.updateMatrix(45.0f, 0.1f, 100.0f);
+
+		camera.Matrix(shaderProgram, "camMatrix");
 
 
 		// Binds texture so that is appears in rendering
